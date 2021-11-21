dbc30e335c52add2b6f651463d19a605bab97144
HackettYam <yeison.molina91@gmail.com>
2021-11-21 06:11:14 -0500

    Fix json api index

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -2,5 +2,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json('Hello World!')
+  res.status(200).json({ title: 'Hello World!' })
 }
